c1bfba06d587b740e438859a494aa2769dcf7a74
Berikisu Ibnrahim <berikisuibrahim227@gmail.com>
2024-10-24 15:28:12 +0000

    made changes

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/user.js";
+import { registerUser, loginUser, getUserProducts, getProfile } from "../controllers/user.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 
 const userRouter = Router();
 
 userRouter.post('/users/register', registerUser);
 userRouter.post('/users/login', loginUser);
+userRouter.get('/users/profile', isAuthenticated, getProfile);
 
+userRouter.get('/users/me/products', isAuthenticated, getUserProducts);
 
 export default userRouter;
